0dcfb88091a85fbc2224f2dc23a4e1ae2b8571e6
Mikhail German <mikhail.german@icloud.com>
2023-01-27 00:37:16 +0500

    Initial Commit

---
+++ TwoIdenticalViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoIdenticalViews
+//
+//  Created by Михаил Герман on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
